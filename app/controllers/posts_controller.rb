e83879880589fe6154c685ee8b932dae79544d47
Asuka217 <asuka375@gmail.com>
2020-10-12 15:31:49 +0900

    AjexAppの準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,7 @@
 class PostsController < ApplicationController
   def index
     # @post = Post.find(1) #一番目のレコードを@postに代入
-    @posts = Post.all #すべてのレコードを＠postsに代入.複数のデータなのでPosts
-  end
-
-  def new
+    @posts = Post.all.order(id: "DESC") #すべてのレコードを＠postsに代入.複数のデータなのでPosts
   end
 
   def create
